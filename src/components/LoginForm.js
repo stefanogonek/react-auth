0cfbe1bb60c474f24abf8d7e23f4cb9ea17f9e40
Stefan Ogonek <stefan.ogonek@gmail.com>
2018-02-28 20:26:23 +0100

    section 9

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
+  state = { email: '', password: '' };
+  
   render() {
     return (
       <Card>
         <CardSection>
+          <Input 
+            placeholder='user@gmail.com'
+            label='Email'
+            value={this.state.email}
+            onChangeText={text => this.setState({ email: text })}
+          />
         </CardSection>
 
         <CardSection>
+          <Input 
+            secureTextEntry
+            placeholder='password'
+            label='Password'
+            value={this.state.password}
+            onChangeText={text => this.setState({ password: text })}
+          />
         </CardSection>
 
         <CardSection>
