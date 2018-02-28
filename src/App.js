0f088ff0c65145e8bf11f1afa292aa8b8ff08ef0
Stefan Ogonek <stefan.ogonek@gmail.com>
2018-02-28 18:15:34 +0100

    lecture 60

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+        apiKey: 'AIzaSyCoahYMx3w3ku7oGAGnquDBtqBOzVg1pwE',
+        authDomain: 'authentication-fe903.firebaseapp.com',
+        databaseURL: 'https://authentication-fe903.firebaseio.com',
+        projectId: 'authentication-fe903',
+        storageBucket: 'authentication-fe903.appspot.com',
+        messagingSenderId: '495532807816'
+    });
+  }
+
   render() {
     return (
       <View>
         <Header headerText='Authentication' />
-        <Text>Yo yo</Text>
+        <LoginForm />
       </View>
     );
   }
